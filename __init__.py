6b989ce114b3f8ad6a7fd02e9b9091a9e9619ea4
RodinLuo <derekluo@users.noreply.github.com>
2025-01-28 14:15:41 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .PromptExpander_node import PromptExpanderNode
+
+NODE_CLASS_MAPPINGS = {"PromptExpanderNode": PromptExpanderNode}
+NODE_DISPLAY_NAME_MAPPINGS = {"PromptExpanderNode": "PromptExpander"}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
